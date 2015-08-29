d6597fbe468c601284a8fab39ece3587cc3558af
Harshit Imudianda <himudianda@apple.com>
2015-08-28 21:48:29 -0700

    More example added for multiple decorators

--- Python/decorators/simple_example.py
+++ Python/decorators/simple_example.py
@@ -1,5 +1,5 @@
 
-
+# Simple decorator example
 def tags_decor(func):
 	def func_wrapper(name):
 		return "<p>%s</p>" % (func(name))
@@ -9,8 +9,42 @@ def tags_decor(func):
 def get_text(name):
 	return "Hello %s" % (name)
 
+
+
+
+
+# Multiple Decorators example
+
+def p_tags_decor(func):
+	def func_wrapper(name):
+		return "<p>%s</p>" % (func(name))
+	return func_wrapper
+
+
+def div_tags_decor(func):
+	def func_wrapper(name):
+		return "<div>%s</div>" % (func(name))
+	return func_wrapper
+
+
+def h1_tags_decor(func):
+	def func_wrapper(name):
+		return "<h1>%s</h1>" % (func(name))
+	return func_wrapper
+
+@p_tags_decor
+@div_tags_decor
+@h1_tags_decor
+def get_text2(name):
+	return "Hello %s" % (name)
+
+
+
+
+
 def main():
 	print get_text("Harshit")
+	print get_text2("Hemant")
 
 if __name__ == "__main__":
 	main()
