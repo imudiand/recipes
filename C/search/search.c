1129f961472c0f43dea4a0ad2489be4e1d852fc6
Harshit Imudianda <himudianda@apple.com>
2015-08-30 00:54:01 -0700

    recursive binary search

--- C/search/search.c
+++ C/search/search.c
@@ -18,7 +18,7 @@ int binary_search_iterative(int *list, int size, int num) {
 	int start=0, end=size, mid;
 
 	while(1){
-		mid = (end-start)/2;
+		mid = (end+start)/2;
 		if (num == list[mid]) {
 			return mid;
 		} else if (num < list[mid]) {
@@ -27,7 +27,17 @@ int binary_search_iterative(int *list, int size, int num) {
 			start = mid+1;
 		}
 	}
+}
 
+int binary_search_recursive(int *list, int start, int end, int num) {
+	int mid = (end+start)/2;
+	if (num == list[mid]) {
+		return mid;
+	} else if (num < list[mid]) {
+		return binary_search_recursive(list, start, mid-1, num);
+	} else {
+		return binary_search_recursive(list, mid+1, end, num);
+	}
 }
 
 
@@ -36,6 +46,8 @@ int main(void) {
 
 	printf("%d\n", linear_search(list, 10, 6));
 	printf("%d\n", binary_search_iterative(list, 10, 6));
+
+	printf("%d\n", binary_search_recursive(list, 0, 9, 6));
 
 
 	return 0;
